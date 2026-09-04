4d1c73142918eba2bf52017cb3000aebabb73c45
Jimo-potato <76jirou@gmail.com>
2026-09-04 22:18:59 +0900

    B - Lucas Number.cpp

--- B - Lucas Number.cpp
+++ B - Lucas Number.cpp
@@ -1,9 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int Lucas_Number(int n) {
-  int Lucas = 0;
-
+int64_t Lucas_Number(int n) {
+  int64_t a[86];
+  a[0] = 2;
+  a[1] = 1;
+  
   if (n == 0) {
-    Lucas = 2;
+    return 2;
   }
+  if (n == 1) {
+    return 1;
+  }
+  
+  for (int i = 2; i <= n; i++) {
+    a[i] = a[i - 1] + a[i - 2];
+  }
+  return a[n];
+}
+
+int main() {
+  int N;
+  cin >> N;
+  
+  cout << Lucas_Number(N) << endl;
+}
